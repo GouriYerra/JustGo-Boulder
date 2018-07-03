f5af3d6025e4b73b173dc20afa0a458c568ddb55
wardkr <kyle.ward@colorado.edu>
2018-07-03 12:17:14 -0600

    compiled attractions/addresses tables, finished comms between front/back

--- emptyDB.sql
+++ emptyDB.sql
@@ -22,33 +22,29 @@ CREATE TABLE AdminsUsersInfo (
     FOREIGN KEY (adminID) REFERENCES Admins(adminID),
     FOREIGN KEY (userID) REFERENCES Users(userID)
     );
-CREATE TABLE Addresses(
-	addressID int auto_increment,
+CREATE TABLE Attractions (
+	attractionID int auto_increment, 
+    attractionName varchar(100) NOT NULL, 
+    category varchar(25) NOT NULL,
+    description varchar(256) NOT NULL,
+    picture varchar(256),
+    phoneNumber varchar(13),
+    pricing int NOT NULL,
     streetNumber int,
     streetName varchar(50),
     unitNumber varchar(10),
     city varchar(25),
     state varchar(2),
     zip varchar(5),
-    PRIMARY KEY (addressID)
-    );
-CREATE TABLE Attractions (
-	attractionID int auto_increment, 
-    attractionName varchar(100) NOT NULL, 
-    category varchar(25) NOT NULL,
-    description varchar(256) NOT NULL,
-    addressID int,
-    picture varchar(256),
-    phoneNumber varchar(13),
-    pricing int NOT NULL,
-    PRIMARY KEY (attractionID),
-    FOREIGN KEY (addressID) REFERENCES Addresses(addressID)
+    PRIMARY KEY (attractionID)
+    #FOREIGN KEY (attractionID) REFERENCES Addresses(addressID)
     );
 CREATE TABLE Comments(
 	commentID int auto_increment,
     commentTime timestamp DEFAULT current_timestamp,
     userID int,
     attractionID int,
+    comment varchar(256),
     primary key (commentID),
     foreign key (userID) references Users(userID),
     FOREIGN KEY (attractionID) references Attractions(attractionID)
